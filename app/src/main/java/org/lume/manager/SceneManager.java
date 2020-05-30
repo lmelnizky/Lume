244bd04491bc15b618c2316e09a49dc07adba71f
Lukas Melnizky <lukas.melnizky@gmail.com>
2020-05-30 22:58:47 +0200

    m

--- app/src/main/java/org/lume/manager/SceneManager.java
+++ app/src/main/java/org/lume/manager/SceneManager.java
@@ -444,10 +444,10 @@ public class SceneManager {
         loadingScene = new LoadingScene();
         setScene(loadingScene);
         ResourcesManager.getInstance().unloadMenuTextures();
+        ResourcesManager.getInstance().loadOnlineUserResources();
         mEngine.registerUpdateHandler(new TimerHandler(1f, new ITimerCallback() {
             @Override
             public void onTimePassed(TimerHandler pTimerHandler) {
-                ResourcesManager.getInstance().loadOnlineUserResources();
                 mEngine.unregisterUpdateHandler(pTimerHandler);
                 onlineUsersScene = MultiplayerUsersScene.getInstance();
                 setScene(onlineUsersScene);
